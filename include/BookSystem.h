3f029e31d5881f142d885bef7edf2bcec994b552
DeliciousBudding <2991573049@qq.com>
2024-12-20 22:59:34 +0800

    重构BookSystem以及Book.json

--- include/BookSystem.h
+++ include/BookSystem.h
@@ -1,34 +1,46 @@
 #ifndef BOOKSYSTEM_H
 #define BOOKSYSTEM_H
+
 #include <bits/stdc++.h>
 #include "nlohmann/json.hpp"
+
 using json = nlohmann::json;
 using namespace std;
+
 class Book // 单本图书的类
 {
-
+private:
     // 数据结构
     int id;
-    int BorrowTimes;
-    int Current;
-    int Total;
-    double Price;
-    string BookCode;
-    string Name;
-    string Author;
-    string Publisher;
-    fstream file;   // 文件流对象，以供整个类使用
-    json Book_Json; // json对象，以供整个类使用
+    int borrowTimes;
+    int current;
+    int total;
+    double price;
+    string bookCode;
+    string name;
+    string originalName;
+    string author;
+    string originalAuthor;
+    string isbnIssn;
+    string publisher;
+    string language;
+    int edition;
+    int publicationYear;
+    int publicationMonth;
+    string introduction;
+
+    fstream file;  // 文件流对象，以供整个类使用
+    json bookJson; // json对象，以供整个类使用
 
 public:
-    Book(const json &OpenedBook); // 构造函数
+    Book(const json &openedBook); // 构造函数
 
-    void ReadData(const json &OpenedBook); // 初始化数据 被所有open方法调用
+    void readData(const json &openedBook); // 初始化数据 被所有open方法调用
     // open方法 打开并读取json文件中的数据 然后获得一个theBook对象
     void open();
     void openId(int id);                        // 以id为参数读取json文件 最基础，其他的open方法都是在此基础上实现的
-    void openCode(const string &InputBookCode); // 以图书编号为参数读取json文件
-    void openName(string &Name);                // 以图书名称为参数读取json文件
+    void openCode(const string &inputBookCode); // 以图书编号为参数读取json文件
+    void openName(const string &name);          // 以图书名称为参数读取json文件
 
     // 操作数据
     void show();   // 显示数据
@@ -37,36 +49,60 @@ public:
     void setAll(); // 设置数据
 
     // 借阅和归还
-    void Borrow(); // 借书
-    void Return(); // 还书
+    void borrow();     // 借书
+    void returnBook(); // 还书
 
     // Getter接口和Setter接口
     int getId() const { return id; }
     void setId(int newId) { id = newId; }
 
-    int getBorrowTimes() const { return BorrowTimes; }
-    void setBorrowTimes(int times) { BorrowTimes = times; }
+    int getBorrowTimes() const { return borrowTimes; }
+    void setBorrowTimes(int times) { borrowTimes = times; }
 
-    int getCurrent() const { return Current; }
-    void setCurrent(int current) { Current = current; }
+    int getCurrent() const { return current; }
+    void setCurrent(int current) { this->current = current; }
 
-    int getTotal() const { return Total; }
-    void setTotal(int total) { Total = total; }
+    int getTotal() const { return total; }
+    void setTotal(int total) { this->total = total; }
 
-    double getPrice() const { return Price; }
-    void setPrice(double price) { Price = price; }
+    double getPrice() const { return price; }
+    void setPrice(double price) { this->price = price; }
 
-    string getBookCode() const { return BookCode; }
-    void setBookCode(const string &code) { BookCode = code; }
+    string getBookCode() const { return bookCode; }
+    void setBookCode(const string &code) { bookCode = code; }
 
-    string getName() const { return Name; }
-    void setName(const string &name) { Name = name; }
+    string getName() const { return name; }
+    void setName(const string &name) { this->name = name; }
 
-    string getAuthor() const { return Author; }
-    void setAuthor(const string &author) { Author = author; }
+    string getOriginalName() const { return originalName; }
+    void setOriginalName(const string &originalName) { this->originalName = originalName; }
 
-    string getPublisher() const { return Publisher; }
-    void setPublisher(const string &publisher) { Publisher = publisher; }
+    string getAuthor() const { return author; }
+    void setAuthor(const string &author) { this->author = author; }
+
+    string getOriginalAuthor() const { return originalAuthor; }
+    void setOriginalAuthor(const string &originalAuthor) { this->originalAuthor = originalAuthor; }
+
+    string getIsbnIssn() const { return isbnIssn; }
+    void setIsbnIssn(const string &isbnIssn) { this->isbnIssn = isbnIssn; }
+
+    string getPublisher() const { return publisher; }
+    void setPublisher(const string &publisher) { this->publisher = publisher; }
+
+    string getLanguage() const { return language; }
+    void setLanguage(const string &language) { this->language = language; }
+
+    int getEdition() const { return edition; }
+    void setEdition(int edition) { this->edition = edition; }
+
+    int getPublicationYear() const { return publicationYear; }
+    void setPublicationYear(int publicationYear) { this->publicationYear = publicationYear; }
+
+    int getPublicationMonth() const { return publicationMonth; }
+    void setPublicationMonth(int publicationMonth) { this->publicationMonth = publicationMonth; }
+
+    string getIntroduction() const { return introduction; }
+    void setIntroduction(const string &introduction) { this->introduction = introduction; }
 };
 
 #endif
--- src/BookSystem.cpp
+++ src/BookSystem.cpp
@@ -1,32 +1,53 @@
 #include <bits/stdc++.h>
 #include "BookSystem.h"
 #include "nlohmann/json.hpp"
+
 using json = nlohmann::json;
 using namespace std;
-Book::Book(const json &OpenedBook) // 构造函数,初始化数据成员 当需要新建Book时使用
+
+// 构造函数, 初始化数据成员 当需要新建Book时使用
+Book::Book(const json &openedBook)
 {
-    id = OpenedBook.value("id", 0);
-    Name = OpenedBook.value("name", "");
-    BookCode = OpenedBook.value("ISBN/ISSN", "");
-    Author = OpenedBook.value("author", "");
-    Publisher = OpenedBook.value("publisher", "");
-    Price = OpenedBook.value("price", 0.00);
-    Total = OpenedBook.value("Total", 0);
-    Current = OpenedBook.value("Current", 0);
-    BorrowTimes = OpenedBook.value("BorrowTimes", 0);
+    id = openedBook.value("id", 0);
+    name = openedBook.value("Name", "");
+    bookCode = openedBook.value("ISBN/ISSN", "");
+    author = openedBook.value("Author", "");
+    originalAuthor = openedBook.value("OriginalAuthor", "");
+    isbnIssn = openedBook.value("ISBN/ISSN", "");
+    publisher = openedBook.value("Publisher", "");
+    language = openedBook.value("Language", "");
+    edition = openedBook.value("Edition", 0);
+    publicationYear = openedBook.value("PublicationYear", 0);
+    publicationMonth = openedBook.value("PublicationMonth", 0);
+    price = openedBook.value("Price", 0.00);
+    total = openedBook.value("Total", 0);
+    current = openedBook.value("Current", 0);
+    borrowTimes = openedBook.value("BorrowTimes", 0);
+    introduction = openedBook.value("Introduction", "");
 }
-void Book::ReadData(const json &OpenedBook)
+
+// 初始化数据 被所有open方法调用
+void Book::readData(const json &openedBook)
 {
-    id = OpenedBook.value("id", 0);
-    Name = OpenedBook.value("name", "");
-    BookCode = OpenedBook.value("ISBN/ISSN", "");
-    Author = OpenedBook.value("author", "");
-    Publisher = OpenedBook.value("publisher", "");
-    Price = OpenedBook.value("price", 0.00);
-    Total = OpenedBook.value("Total", 0);
-    Current = OpenedBook.value("Current", 0);
-    BorrowTimes = OpenedBook.value("BorrowTimes", 0);
+    id = openedBook.value("id", 0);
+    name = openedBook.value("Name", "");
+    bookCode = openedBook.value("ISBN/ISSN", "");
+    author = openedBook.value("Author", "");
+    originalAuthor = openedBook.value("OriginalAuthor", "");
+    isbnIssn = openedBook.value("ISBN/ISSN", "");
+    publisher = openedBook.value("Publisher", "");
+    language = openedBook.value("Language", "");
+    edition = openedBook.value("Edition", 0);
+    publicationYear = openedBook.value("PublicationYear", 0);
+    publicationMonth = openedBook.value("PublicationMonth", 0);
+    price = openedBook.value("Price", 0.00);
+    total = openedBook.value("Total", 0);
+    current = openedBook.value("Current", 0);
+    borrowTimes = openedBook.value("BorrowTimes", 0);
+    introduction = openedBook.value("Introduction", "");
 }
+
+// 打开并读取json文件中的数据
 void Book::open()
 {
     // 如果已经打开，直接返回
@@ -39,159 +60,230 @@ void Book::open()
     {
         cerr << "无法打开文件 Book.json" << endl;
     }
-    return;
 }
+
+// 关闭文件
 void Book::close()
 {
     if (file.is_open()) // 只有打开了才关闭
         file.close();
 }
+
+// 以id为参数读取json文件 最基础，其他的open方法都是在此基础上实现的
 void Book::openId(int id)
 {
     open();
 
     // 读取文件内容到json对象
-    json Book_Json;
-    file >> Book_Json;
+    json bookJson;
+    file.seekg(0); // 确保文件指针在文件开头
+    file >> bookJson;
 
     // 检查输入的书序号是否有效
-    if (id < 1 || id > Book_Json.size())
+    if (id < 1 || id > bookJson.size())
     {
-        cerr << "无效的书序号，请输入1到" << Book_Json.size() << "之间的数字" << endl;
+        cerr << "无效的书序号，请输入1到" << bookJson.size() << "之间的数字" << endl;
         return;
     }
 
     // 将书序号转换为字符串，因为json对象的键是字符串类型
     string bookKey = to_string(id);
 
-    // 获取并打印第n本书的信息
-    if (Book_Json.contains(bookKey))
+    // 获取并读取第n本书的信息
+    if (bookJson.contains(bookKey))
     {
-        ReadData(Book_Json[bookKey]); // 读取数据到成员变量
+        readData(bookJson[bookKey]); // 读取数据到成员变量
     }
     else
     {
         cerr << "未找到指定序号的书" << endl;
         return;
     }
-
-    return;
 }
-void Book::openCode(const string &InputBookCode)
+
+// 以图书编号为参数读取json文件
+void Book::openCode(const string &inputBookCode)
 {
     open();
-    json j = json::parse(file);
-    for (auto &i : j)
+    json bookJson;
+    file.seekg(0); // 确保文件指针在文件开头
+    file >> bookJson;
+
+    for (auto &item : bookJson.items())
     {
-        if (i["ISBN/ISSN"] == InputBookCode)
+        if (item.value()["ISBN/ISSN"] == inputBookCode)
         {
-            ReadData(i); // 读取数据到成员变量 i即为id
+            readData(item.value()); // 读取数据到成员变量
             return;
         }
     }
     cerr << "未找到指定书号的书" << endl;
 }
-void Book::openName(string &Name)
+
+// 以图书名称为参数读取json文件
+void Book::openName(const string &name)
 {
     open();
-    json j = json::parse(file);
-    for (auto &i : j)
+    json bookJson;
+    file.seekg(0); // 确保文件指针在文件开头
+    file >> bookJson;
+
+    for (auto &item : bookJson.items())
     {
-        if (i["书名"] == Name)
+        if (item.value()["Name"] == name)
         {
-            ReadData(i); // 读取数据到成员变量 i即为id
+            readData(item.value()); // 读取数据到成员变量
             return;
         }
     }
     cerr << "未找到指定书名的书" << endl;
 }
+
+// 显示数据
 void Book::show()
 {
     cout << endl;
-    cout << "1.书名：" << Name << endl;
-    cout << "2.书号ISBN/ISSN：" << BookCode << endl;
-    cout << "3.作者：" << Author << endl;
-    cout << "4.出版社：" << Publisher << endl;
-    cout << "5.价格：" << Price << endl;
-    cout << "6.总数：" << Total << endl;
-    cout << "7.当前库存：" << Current << endl;
-    cout << "8.借阅次数：" << BorrowTimes << endl;
+    cout << "1. 书名：" << name << endl;
+    cout << "2. 原书名：" << originalName << endl;
+    cout << "3. 作者：" << author << endl;
+    cout << "4. 原作者：" << originalAuthor << endl;
+    cout << "5. ISBN/ISSN：" << isbnIssn << endl;
+    cout << "6. 出版社：" << publisher << endl;
+    cout << "7. 语言：" << language << endl;
+    cout << "8. 版本：" << edition << endl;
+    cout << "9. 出版年份：" << publicationYear << endl;
+    cout << "10. 出版月份：" << publicationMonth << endl;
+    cout << "11. 价格：" << price << endl;
+    cout << "12. 总数：" << total << endl;
+    cout << "13. 当前库存：" << current << endl;
+    cout << "14. 借阅次数：" << borrowTimes << endl;
+    cout << "15. 简介：" << introduction << endl;
 }
+
+// 保存数据
 void Book::save()
 {
-    // 将Book类的数据存入Book_Json
+    // 将Book类的数据存入bookJson
     json tempBook;
-    tempBook["BorrowTimes"] = BorrowTimes;
-    tempBook["Current"] = Current;
-    tempBook["ISBN/ISSN"] = BookCode;
-    tempBook["Total"] = Total;
-    tempBook["author"] = Author;
-    tempBook["name"] = Name;
-    tempBook["price"] = Price;
-    tempBook["publisher"] = Publisher;
     tempBook["id"] = id;
+    tempBook["Name"] = name;
+    tempBook["OriginalName"] = originalName;
+    tempBook["Author"] = author;
+    tempBook["OriginalAuthor"] = originalAuthor;
+    tempBook["ISBN/ISSN"] = isbnIssn;
+    tempBook["Publisher"] = publisher;
+    tempBook["Language"] = language;
+    tempBook["Edition"] = edition;
+    tempBook["PublicationYear"] = publicationYear;
+    tempBook["PublicationMonth"] = publicationMonth;
+    tempBook["Price"] = price;
+    tempBook["Total"] = total;
+    tempBook["Current"] = current;
+    tempBook["BorrowTimes"] = borrowTimes;
+    tempBook["Introduction"] = introduction;
 
-    Book_Json[to_string(id)] = tempBook; // j[id] = tempBook; 相当于j["id"] = tempBook;
-    // 对对象j进行存储
-    file << Book_Json.dump(4) << endl;
+    bookJson[to_string(id)] = tempBook; // 将数据存入json对象
+
+    // 将json对象写入文件
+    file.seekp(0); // 确保文件指针在文件开头
+    file << bookJson.dump(4) << endl;
+    file.flush(); // 刷新缓冲区
 
     cout << "数据已保存" << endl
          << endl;
 }
+
+// 设置数据
 void Book::setAll()
 {
-    bool exitOfsetAll = false;
-    while (!exitOfsetAll)
+    bool exitOfSetAll = false;
+    while (!exitOfSetAll)
     {
         show();
         cout << endl;
         cout << "请输入要修改的字段序号(输入0退出)：";
-        int chooseOfsetAll = 0;
-        cin >> chooseOfsetAll;
-        switch (chooseOfsetAll)
+        int chooseOfSetAll = 0;
+        cin >> chooseOfSetAll;
+        switch (chooseOfSetAll)
         {
         case 1:
             cout << "请输入新的图书名称：";
-            cin >> BookCode;
-            setBookCode(BookCode);
+            cin >> name;
+            setName(name);
             break;
         case 2:
-            cout << "请输入新的图书ISBN/ISSN：";
-            cin >> Name;
-            setName(Name);
+            cout << "请输入新的原书名：";
+            cin >> originalName;
+            setOriginalName(originalName);
             break;
         case 3:
             cout << "请输入新的作者：";
-            cin >> Author;
-            setAuthor(Author);
+            cin >> author;
+            setAuthor(author);
             break;
         case 4:
-            cout << "请输入新的出版社：";
-            cin >> Publisher;
-            setPublisher(Publisher);
+            cout << "请输入新的原作者：";
+            cin >> originalAuthor;
+            setOriginalAuthor(originalAuthor);
             break;
         case 5:
-            cout << "请输入新的价格：";
-            cin >> Price;
-            setPrice(Price);
+            cout << "请输入新的ISBN/ISSN：";
+            cin >> isbnIssn;
+            setIsbnIssn(isbnIssn);
             break;
         case 6:
-            cout << "请输入新的总数：";
-            cin >> Total;
-            setTotal(Total);
+            cout << "请输入新的出版社：";
+            cin >> publisher;
+            setPublisher(publisher);
             break;
         case 7:
-            cout << "请输入新的当前库存：";
-            cin >> Current;
-            setCurrent(Current);
+            cout << "请输入新的语言：";
+            cin >> language;
+            setLanguage(language);
             break;
         case 8:
+            cout << "请输入新的版本：";
+            cin >> edition;
+            setEdition(edition);
+            break;
+        case 9:
+            cout << "请输入新的出版年份：";
+            cin >> publicationYear;
+            setPublicationYear(publicationYear);
+            break;
+        case 10:
+            cout << "请输入新的出版月份：";
+            cin >> publicationMonth;
+            setPublicationMonth(publicationMonth);
+            break;
+        case 11:
+            cout << "请输入新的价格：";
+            cin >> price;
+            setPrice(price);
+            break;
+        case 12:
+            cout << "请输入新的总数：";
+            cin >> total;
+            setTotal(total);
+            break;
+        case 13:
+            cout << "请输入新的当前库存：";
+            cin >> current;
+            setCurrent(current);
+            break;
+        case 14:
             cout << "请输入新的借阅次数：";
-            cin >> BorrowTimes;
-            setBorrowTimes(BorrowTimes);
+            cin >> borrowTimes;
+            setBorrowTimes(borrowTimes);
+            break;
+        case 15:
+            cout << "请输入新的简介：";
+            cin.ignore(); // 忽略前一个换行符
+            getline(cin, introduction);
+            setIntroduction(introduction);
             break;
         case 0:
-            exitOfsetAll = true;
+            exitOfSetAll = true;
             break;
         default:
             cout << "无效的选择，请重新输入" << endl;
@@ -199,20 +291,35 @@ void Book::setAll()
     }
     save();
 }
-void Book::Borrow()
+
+// 借书
+void Book::borrow()
 {
-    if (Current > 0)
+    if (current > 0)
     {
-        Current--;
-        BorrowTimes++;
+        current--;
+        borrowTimes++;
         save();
     }
-}
-void Book::Return()
-{
-    if (Current < Total)
+    else
     {
-        Current++;
+        cout << "当前库存不足，无法借书" << endl;
+    }
+}
+
+// 还书
+void Book::returnBook()
+{
+    if (current < total)
+    {
+        current++;
         save();
+    }
+    else
+    {
+        total++;
+        current++;
+        save();
+        cout << "库存增加" << endl;
     }
 }
